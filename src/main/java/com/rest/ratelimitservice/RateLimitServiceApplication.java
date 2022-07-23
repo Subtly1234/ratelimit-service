077e425efe30d39b07b637aff3d99587fd7113f5
lzj <77909821+Subtly1234@users.noreply.github.com>
2022-07-24 01:57:30 +0800

    fix

--- src/main/java/com/rest/ratelimitservice/RateLimitServiceApplication.java
+++ src/main/java/com/rest/ratelimitservice/RateLimitServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RateLimitServiceApplication {
 
     public static void main(String[] args) {
+        String ip = System.getenv("MY_NODE_NAME");
+        System.err.println(ip);
         SpringApplication.run(RateLimitServiceApplication.class, args);
     }
 
